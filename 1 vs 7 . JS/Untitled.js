7a7070f725b9899e47414f145f835ece66b45683
sarvar <sarlion.98@mail.ru>
2024-07-31 16:09:18 +0300

    js

---
+++ 1 vs 7 . JS/Untitled.js
@@ -0,0 +1,14 @@
+//alert('Hello JavaScript')
+//console.log(1)
+// Ozgaruvchanlar uch turga bolinadi birinchisiz
+//LET va VAR kalet soz ozgaruvchan <const> ozgarmas 
+let ism = 'Ahror'
+console.log(ism)
+
+const familiya = 'Soliyev'
+
+console.log(familiya)
+
+var jay = 'Fargona'
+jay = 'Fargona tumani'
+console.log(jay)
