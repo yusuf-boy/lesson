759ae5d600385f1b5b0402f7aedb592b43260b15
sarvar <sarlion.98@mail.ru>
2024-08-01 16:24:45 +0300

    js

---
+++ 1 vs 7 . JS/8 .JS/Untitled.js
@@ -0,0 +1,27 @@
+// string
+console.log('Hello world wefWEF@ #$!#')
+
+let email = 'akrorweb@gmail.com'
+console.log(email)
+
+//strinh concatenation < ikkita stringni + >
+let firstName = 'Ahror'
+let lastName = 'Soliyer'
+
+let fullName = firstName +' '+ lastName
+console.log(fullName)
+
+// getting characters
+console.log(fullName[0])
+
+// string Length => uzunlik
+console.log(fullName.length)
+
+// string methods => funktsiyaga o'xshash ish bajaradi
+console.log(fullName.toUpperCase())
+
+let result = fullName.toLowerCase()
+console.log(result)
+
+let index = email.indexOf('@')
+console.log(index)
