513d0d14b2a5e4d68b8331e2a4242796c8bc64b9
Your Name <you@example.com>
2024-08-18 21:47:48 +0500

    ergeaeh

---
+++ js/Untitled.js
@@ -0,0 +1,32 @@
+let age = 25; // Declare and assign a variable [O'zgaruvchini e'lon qiling va tayinlang]
+const name = "yusuf"; // Declare a constant variable [Doimiy o'zgaruvchini e'lon qilish]
+
+let num = 42;        // Nunber
+let str = 'hello';   // String
+let flag = true;     // Boolean
+let obj = { key: "value" };  // Object
+let arr = [1, 2, 3]; // Array
+
+let result = 5 + 3;           // Addition
+let isGreaterThan = 54 > 3;   // Comparison
+let isTrue = true && false;   // logicar
+
+/*if (condition) {
+   // Code to execute if condition is true [ Agar shart to'g'ri bo'lsa, bajariladigan kod ]
+} else if (anotherCondition) {
+    // Code to execute if anotherCondition is true [ Agar boshqa shart to'g'ri bo'lsa, bajariladigan kod ]
+} else {
+    // Code to execute if no condition is true [ Hech qanday shart bajarilmasa, bajariladigan kod ]
+}*/
+
+/*for(let i = 0; i < 5; i++) {
+    // Code to repeat 5 times [ 5 marta takrorlash uchun kod ]
+}
+
+while (condition) {
+    // Code to repeat while condition is true  [ Shart to'g'ri bo'lganda takrorlash uchun kod ]
+} */
+
+
+
+console.log(isGreaterThan)
