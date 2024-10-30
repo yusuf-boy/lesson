c275427f51764629761dce805a5a2cf073ce0803
sarvar <sarlion.98@mail.ru>
2024-10-30 07:04:31 +0300

    sfsef3213

---
+++ js/js/cod.js
@@ -0,0 +1,67 @@
+const sayHello = function (name, day = 'kech') {
+    console.log(`Hayrili ${day} ${name}`)
+}
+
+sayHello('behruz', 'tong')
+
+
+
+
+// // const calcArea = function() {
+// //     const area = 3.14 * radius ** 2
+// //     return area
+// //     // console.log(area)
+// // }
+// // const a = calcArea(5)
+// // console.log(a)
+
+// const calcArea = function (radius) {
+//     return 3.14 * radius ** 2 
+// }
+// const a = calcArea(5)
+
+
+
+
+// const calcArea = (radius) => {
+//     return 3.14 * radius ** 2
+// }
+// const area = calcArea(5)
+// console.log(area)
+
+
+// const bill = function (products, tax) {
+//     let total = 0
+
+//     for (let i = 0; i < products.length; i++) {
+//         total += products[i] + products[i] * tax
+//     }
+
+//     return total
+// }
+// const result = bill([10, 13, 45], 0.2)
+// console.log(result)
+
+
+
+
+// //functions vs methods
+
+// const ism = 'ahror'
+
+// //function
+// const greet = () => 'Hello'
+//     const result = greet()
+//     console.log(result)
+
+
+// //methods
+// const res = ism.toUpperCase()
+// console.log(res)
+
+// 34.23243.toFixed()
+
+
+
+
+//Tahlil
