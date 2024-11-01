6ece6ffe3caf2c1191d041636dc4319a724c7cf7
sarvar <sarlion.98@mail.ru>
2024-11-01 09:37:28 +0300

    python

---
+++ py/cod.py
@@ -0,0 +1,48 @@
+x = 5        
+y = 3.14     
+ism = "Ali"
+
+print(type(x))      
+print(type(y))      
+print(type(ism))
+
+
+
+
+x = 10
+y = 5
+
+if x > y:
+    print("x y dan katta")
+else:
+    print("x y dan kichik yoki teng")
+
+
+
+
+
+son = 15
+
+if son > 0:
+    print("Musbat son")
+elif son < 0:
+    print("Manfiy son")
+else:
+    print("Son nolga teng")
+
+
+
+
+raqamlar = [1, 2, 3, 4, 5]
+for raqam in raqamlar:
+    print(raqam)
+
+for i in range(5):
+    print(i)
+
+
+
+
+
+
+
