920382df5201cd632cef80da718b4364dcb6e388
Your Name <you@example.com>
2024-12-01 22:47:07 +0500

    ls

---
+++ js/js/noldan.js
@@ -0,0 +1,21 @@
+// objectlar arrayda
+
+const movies = [
+    {name: 'avatar', likes:456},
+    {name: 'Harry Potter', likes:484},
+    {name: 'Titsnic', likes:987},
+    {name: 'Sitve Jobs', likes:156},
+    {name: 'Sherlock', likes:574},
+]
+
+movies.forEach((movies)=> {
+    // BU BITA TATOR BOLIB CHIGADI
+    // console.log(movies.name)
+    // console.log(movies.likes)
+
+    //BU ESA HAMMASI BITA SOZ BOLIB CHIGADI
+    // const result = `Kino nomi: ${movies.name}, Likes: ${movies.likes}` 
+    // console.log(result);
+})
+
+
