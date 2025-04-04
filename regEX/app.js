a7d849d06ec39231e7282d3a7ccd6e64eeb7eded
yusuf-boy <avlioqulovyusuf96@gmail.com>
2025-04-04 10:34:01 +0500

    second  commit

--- regEX/app.js
+++ regEX/app.js
@@ -1,12 +1,13 @@
 const signupForm = document.querySelector('#signup-form')
 // const username = document.querySelector('#username')
 const message = document.querySelector('.message')
- 
+const regEX = /^[a-zA-Z0-9]{6,12}$/ 
+
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault()
     // console.log('Form Subimed')
     // console.log(username.value)
-    const regEX = /^[a-zA-Z0-9]{6,12}$/
+    // const regEX = /^[a-zA-Z0-9]{6,12}$/
     const inputVal = signupForm.username.value
 
     if(regEX.test(inputVal)) {
@@ -24,3 +25,12 @@ signupForm.addEventListener('submit', (e) => {
 // const result = name.search(regEX)
 
 // console.log(result)
+
+//Live feedbock
+signupForm.username.addEventListener('keyup', (e) => {
+    if (regEX.test(e.target.value)) {
+        signupForm.username.setAttribute('class', 'success')
+    } else {
+        signupForm.username.setAttribute('class', 'error')
+    }
+})
