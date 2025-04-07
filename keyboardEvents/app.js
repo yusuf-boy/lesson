384488287258a42bc32dd59ffe7d11251257a11e
yusuf-boy <avlioqulovyusuf96@gmail.com>
2025-04-07 14:51:30 +0500

    ekamit

--- keyboardEvents/app.js
+++ keyboardEvents/app.js
@@ -3,6 +3,7 @@ document.addEventListener('keyup', (e) => {
     if (e.key == 'm' && e.ctrlKey) {
         console.log('GOOOL!!!')
     }
+
     // console.log('Code', e.code)
     // console.log('Key', e.key)
     // console.log('keyCode', e.keyCode)
