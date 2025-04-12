f88329c53d5f5854b00e722f464bfbe0672efb52
yusuf-boy <avlioqulovyusuf96@gmail.com>
2025-04-12 17:33:05 +0500

    code completed

---
+++ LocalStorage/app.js
@@ -0,0 +1,29 @@
+// LocalStorage
+
+// setItem
+// const names = ['ahror', 'sardor','doniyor']
+// localStorage.setItem('names', JSON.stringify(names))
+//
+// // getItem
+// const getNames = JSON.parse(localStorage.getItem('name'))
+// console.log(getNames)
+//
+// // clear
+// localStorage.clear()
+
+console.log(localStorage.getItem('names'));
+const names = JSON.parse(localStorage.getItem('names')) ? JSON.parse(localStorage.getItem('names')) : []
+
+if (names.length > 0) {
+    names.forEach((name) => {
+        console.log(name)
+    })
+} else {
+    console.log("no name")
+}
+
+
+
+
+
+
---
+++ Ternary operation/app.js
@@ -0,0 +1,21 @@
+// Ternary operation - triad operator
+
+const age = prompt('enter your age:', 18)
+
+/*if (age < 18) {
+    alert('you haven\'t come of age yet!')
+} else {
+    alert('you\'ve come of age!')
+}  */
+
+/*age < 18 ? alert('you haven\'t come of age yet!'):
+    alert('you\'ve come of age!')
+ */
+
+/*//Army age
+age < 18 ? alert(`siz armiya yoshidan ${18 - age} yosh kichiksiz`):
+    age < 28 ? alert('siz armiyaga borishi mumkin!'):
+        alert('siz armiya yoshidan kattasiz')
+ */
+
+
