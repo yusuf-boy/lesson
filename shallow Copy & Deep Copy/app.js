8e2fadd5b868c455e881efcd8ab8b643797a85fd
yusuf-boy <avlioqulovyusuf96@gmail.com>
2025-04-13 18:52:06 +0500

    code completed

---
+++ shallow Copy & Deep Copy/app.js
@@ -0,0 +1,28 @@
+// shallow Copy & Deep Copy
+
+/* -- primitive --*/
+let a = 2
+let b = a
+b = 7
+
+console.log('b', a)
+console.log('a:', b)
+
+/* -- reference --*/
+const person = {
+    name: 'John',
+    age: 32,
+}
+
+// spread operator - to'liq ma'lumotni nusxalash
+const newPerson = {...person}
+
+newPerson.name = 'anna'
+newPerson.age = 28
+
+console.log('person', person)
+console.log('newPerson:', newPerson)
+
+// chap tomonda kelsa bu rest operator qolgan ma'lumontni nusxalash
+const [bir, ...qolgani] = [1, 2, 3]
+console.log(qolgani)
